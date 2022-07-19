1fb75b99f386872e993bc3a71bd5c385e1178314
Mathias Lauridsen <math227v@gmail.com>
2022-07-19 23:34:43 +0200

    :tada: Tailwind support

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-mono text-2xl text-green-800">Shelter Time Vis</h1>
