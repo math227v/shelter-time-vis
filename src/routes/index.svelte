0463e3140f8676a0d42a3dc479ac28fa9ccb0795
Mathias Lauridsen <math227v@gmail.com>
2022-07-19 23:51:03 +0200

    :construction: CI Trigger

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <div class="bg-blue-500 w-full min-h-screen flex">
     <div class="mx-auto my-auto">
-        <h1 class="font-black text-2xl text-white">Shelter time vis</h1>
+        <h1 class="font-black text-2xl text-white">Shelter time vis !</h1>
     </div>
 </div>
