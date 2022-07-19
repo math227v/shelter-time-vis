e1b1fe7f44ebd448caedd579784bec37c58513bc
Mathias Lauridsen <math227v@gmail.com>
2022-07-19 23:28:05 +0200

    :tada: GH Actions for auto build and deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,30 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+// import { dev } from '$app/env';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		},
+		paths: {
+			// base: dev ? "" : '/work-time-vis',
+			base: '/shelter-time-vis',
+		},
+
+	},
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 };
 
 export default config;
